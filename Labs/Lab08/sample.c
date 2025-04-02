b733a2f15cfb6933a21cee014a0082c5d5d6db63
Rensildi <rensildi.kalanxhi1@gmail.com>
2025-04-01 19:11:39 -0500

    Adding mutex locking before and after Pi calculation

--- Labs/Lab08/sample.c
+++ Labs/Lab08/sample.c
@@ -1,3 +1,14 @@
+// Issues with the code:
+/*
+    Has multiple threads = 50
+    Each thread generates random points (x,y) and check if they fall inside the circle
+    g_PI is shared -> concurrent updates from multiple threads can lead to race condtion
+    it will lead to unprecise answer
+
+    mutex lock is needed to make sure threads are updateing one at a time
+    lock must be before modyfing g_PI then release afterward
+*/
+
 #include <sys/types.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -6,6 +17,8 @@
 #include <limits.h>
 #include <math.h>
 #include <time.h>
+
+pthread_mutex_t pi_mutex;
 
 typedef struct{
   int arr_len;
@@ -18,12 +31,18 @@ void* monte_carlo(void* v_as) {
     args_struct* as = v_as;
     int arr_len     = (*as).arr_len;
     for (int i = 0; i < arr_len; i++) {
-        double random_x = rand()/(double)RAND_MAX;
-        double random_y = rand()/(double)RAND_MAX;
-
-        g_PI += random_x*random_x + random_y*random_y <= 1;
+        unsigned int seed = time(NULL) ^ pthread_self();
+        double random_x = rand_r(&seed) / (double)RAND_MAX;
+        double random_y = rand_r(&seed) / (double)RAND_MAX;
+        local_count += (random_x*random_x + random_y*random_y <= 1);
 
     }
+    // locking before modifyign pi
+    // unlocking after modifyign pi
+    pthread_mutex_lock(&pi_mutex);
+    g_PI += local_count;
+    pthread_mutex_unlock(&pi_mutex);
+
     pthread_exit(0);
 }
 
@@ -47,6 +66,9 @@ int main(int argc, char* argv[])
     (*args).arr_len = len;
     v_args = (void*)(args);
 
+    // initializing mutex
+    pthread_mutex_init(&pi_mutex, NULL);
+
     for (i = 0; i < num_threads; i++) {
         if (pthread_create(&thread[i], NULL, &monte_carlo, v_args) != 0) {
             printf("ERROR: Monte Carlo thread created incorrectly.\n");
@@ -61,6 +83,8 @@ int main(int argc, char* argv[])
 
     printf("Pi is: %f\n", pi);
     free(args);
+    // destorying mutex
+    pthread_mutex_destroy(&pi_mutex);
     return 0;
 }
 
